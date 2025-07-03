aa367e82116169a3fd93311264f0a02f6b2df662
emman355 <earevalo355@gmail.com>
2025-07-03 22:52:47 +0800

    feat: implement sign out functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { useAuth } from "@/lib/auth-context";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function Index() {
+  const { signOut } = useAuth()
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
       <Text>Hello this is Home!</Text>
+      <Button mode="text" onPress={signOut} icon={"logout"}>Sign Out</Button>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
